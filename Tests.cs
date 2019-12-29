22f6d4ed6737991939b15c1fd332383791cd4e28
Inna-anris <ivana.anris@gmail.com>
2019-12-29 19:58:33 +0100

    Add project files.

---
+++ Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace ZavrsniTestIvana
+{
+    class Tests : SeleniumBaseClass
+    {
+
+        [Test]
+        public void Zadatak1()
+        {
+
+        }
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            this.Driver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void TearDownTests()
+        {
+            this.Close();
+        }
+    }
+}
